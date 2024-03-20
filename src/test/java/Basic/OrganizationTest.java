3bb4194366abe61f89c2ce2067207a30ca4faa25
Suman-parge <57311208+Suman-parge@users.noreply.github.com>
2024-03-20 11:32:20 +0530

    Update OrganizationTest.java

--- src/test/java/Basic/OrganizationTest.java
+++ src/test/java/Basic/OrganizationTest.java
@@ -5,8 +5,8 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.Test;
 
 import CommanUtils.PropertyFileUtil;
 import CommanUtils.WebDriverUtil;
